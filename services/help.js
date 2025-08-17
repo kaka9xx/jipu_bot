e569ccb55cc7dda86ab0bfc8533797338e7da9ea
truongluy <luylife99@gmail.com>
2025-08-18 01:02:10 +0700

    qqqqqqqqqqqqqqqqqq

--- services/help.js
+++
@@ -1,8 +0,0 @@
-export async function handleHelp(bot, msg, t, lang) {
-  const chatId = msg.chat.id;
-  const text = t(lang, "help_text");
-
-  await bot.sendMessage(chatId, text, {
-    reply_markup: { inline_keyboard: [[{ text: "⬅️ Menu", callback_data: "back_menu" }]] }
-  });
-}
